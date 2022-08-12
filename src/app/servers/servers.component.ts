932b38c81d403601975b9150dc8417f0c9c0812a
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-08-12 12:04:26 -0500

    created server name

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,10 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'testserver';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
+onCreateServer() {
+  this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+ 
+}
+onUpdateServerName(event: Event ) {
+  this.serverName = (<HTMLInputElement>event.target).value;
 
+}
 }
