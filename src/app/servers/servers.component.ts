3c09e5003a2ea0900d9f76c7030694491f730049
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-08-12 09:25:16 -0500

    fixed app-server loading error

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,19 @@ import { Component, OnInit } from '@angular/core';
  /*  selector: '[app-servers]', */
  /* or this could also be '.app-servers' */
  selector: 'app-servers',
-  template: 
-  `<app-server></app-server>
-   <app-server></app-server>`,
+/*   template: `
+   <app-server></app-server>
+   <app-server></app-server>` , */
+   templateUrl: `./servers.component.html`,
+  
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
