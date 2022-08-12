1a2793612744e6bb4eb41eab85e913a1ce0222fa
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-08-12 15:16:09 -0500

    learned ng class and ngstyle

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'testserver';
+  serverCreated = false;
 
   constructor() { 
     setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 onCreateServer() {
+  this.serverCreated = true;
   this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
  
 }
